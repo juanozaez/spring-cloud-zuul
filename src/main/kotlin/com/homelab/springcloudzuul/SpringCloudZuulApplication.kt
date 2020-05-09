fbf84b74e6dd1eb4001d7489647a7d0aa69a0e97
Juanlu <jozaez@gmail.com>
2020-05-09 18:07:15 +0200

    Initial commit

---
+++ src/main/kotlin/com/homelab/springcloudzuul/SpringCloudZuulApplication.kt
@@ -0,0 +1,11 @@
+package com.homelab.springcloudzuul
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringCloudZuulApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringCloudZuulApplication>(*args)
+}
---
+++ src/test/kotlin/com/homelab/springcloudzuul/SpringCloudZuulApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.homelab.springcloudzuul
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringCloudZuulApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
